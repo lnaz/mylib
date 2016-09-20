30a534bc84e93656e5a27aae2d3868681944a534
Kotaro Abe <abe@human.ait.kyushu-u.ac.jp>
2016-09-20 20:30:15 +0900

    fix tools

--- python/mylib/cv2_tools.py
+++ python/mylib/cv2_tools.py
@@ -9,3 +9,15 @@ def display_img(img):
 def img_path_to_mat(path):
     img  = cv2.imread(path, -1)
     return img
+
+def read_imgs(filepaths):
+    imgs = []
+    for path in filepaths:
+        img = cv2.imread(path, -1)
+        imgs.append(img)
+    return imgs
+
+def write_imgs(imgs, filepaths):
+    for img, path in zip(imgs, filepaths):
+        cv2.imwrite(path, img)
+
--- python/mylib/tools.py
+++ python/mylib/tools.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import glob
 
 # ディレクトリが存在しない場合のみ作成
 def make_dir(path):
@@ -21,7 +22,7 @@ def a2n(alph):
     return ord(alph) - 65
 
 # 文字列リストを連結して返す
-def list2str(ls):
+def list2path(ls):
     st = ''
     for l in ls:
         st += str(l) + '/'
@@ -41,7 +42,7 @@ def make_num_dir(path):
         num_path_list = []
         num_path_list = cor_path_sp[:]
         num_path_list.append(dir_name + str(i))
-        num_path = list2str(num_path_list)
+        num_path = list2path(num_path_list)
         if not os.path.exists(num_path):
             os.makedirs(num_path)
             break
@@ -51,3 +52,32 @@ def make_num_dir(path):
 def make_alphabets():
     alphs = [chr(i + 65) for i in xrange(26)]
     return alphs
+
+# パスのディレクトリ内のファイルのパスを取得
+def get_filepaths(dirpath, findname):
+    cor_dirpath = correct_dir_path(dirpath)
+    filepaths = glob.glob(cor_dirpath + findname)
+    return filepaths
+
+def set_filepaths(filepaths, addition):
+    added_filepaths = []
+    for path in filepaths:
+        path_sp = path.split('.')
+        ext = '.' + path_sp[-1]
+        added_path = path.rstrip(ext)
+        added_path += addition
+        added_path += ext
+        added_filepaths.append(added_path)
+    return added_filepaths
+
+def set_special_filepaths(filepaths, additions):
+    added_filepaths = []
+    for (path, addition) in zip(filepaths, additions):
+        path_sp = path.split('.')
+        ext = '.' + path_sp[-1]
+        added_path = path.rstrip(ext)
+        added_path += addition
+        added_path += ext
+        added_filepaths.append(added_path)
+    return added_filepaths
+    
